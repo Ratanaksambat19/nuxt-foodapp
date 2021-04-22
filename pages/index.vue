cb766678025771ccb907dadd25612abadbdd4fd0
doung ratanaksambat <doung.ratanaksambat19@kit.edu.kh>
2021-04-22 21:12:26 +0700

    working project

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="container">
+    <AppHeader/>
+    
+  </main>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/restaurants.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container restaurant">
+    <h1>Hello restaurant</h1>
+    <AppRestaurantInfo />
+  </div>
+</template>
+
+<script>
+import AppRestaurantInfo from '@/components/AppRestaurantInfo.vue';
+export default {
+  components: {
+    AppRestaurantInfo,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
